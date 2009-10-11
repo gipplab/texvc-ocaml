764ac293f60a2f8f36651691193b25aad381f4b7
Ralf Schmitt <ralf@systemexit.de>
2009-10-11 23:58:14 +0200

    allow to pass resolution
    Comment from @physikerwelt:
    This was originally implemented as 5'th argument.
    But since backgroud color was introduced it's now
    argument 6.

--- texvc.ml
+++ texvc.ml
@@ -7,23 +7,23 @@ let lexer_token_safe lexbuf =
     with Failure s -> raise (LexerException s)
 
 (* *)
-let render tmppath finalpath tree backcolor =
+let render tmppath finalpath tree backcolor resolution_in_dpi =
     let outtex = Util.mapjoin Texutil.render_tex tree in
     let md5 = Digest.to_hex (Digest.string outtex) in
     begin
-	let mathml = Mathml.render tree
-	and html = Html.render tree
-	in print_string (match (html,!Html.conservativeness,mathml) with
-	    None,_,None -> "+" ^ md5 
-	  | Some h,Html.CONSERVATIVE,None -> "c" ^ md5  ^ h
-	  | Some h,Html.MODERATE,None -> "m" ^ md5  ^ h
-	  | Some h,Html.LIBERAL,None -> "l" ^ md5  ^ h
-	  | Some h,Html.CONSERVATIVE,Some m -> "C" ^ md5  ^ h ^ "\000" ^ m
-	  | Some h,Html.MODERATE,Some m -> "M" ^ md5  ^ h ^ "\000" ^ m
-	  | Some h,Html.LIBERAL,Some m -> "L" ^ md5 ^ h ^ "\000" ^ m
-	  | None,_,Some m -> "X" ^ md5   ^ m
-	);
-	Render.render tmppath finalpath outtex md5 backcolor
+    let mathml = Mathml.render tree
+    and html = Html.render tree
+    in print_string (match (html,!Html.conservativeness,mathml) with
+        None,_,None -> "+" ^ md5
+      | Some h,Html.CONSERVATIVE,None -> "c" ^ md5  ^ h
+      | Some h,Html.MODERATE,None -> "m" ^ md5  ^ h
+      | Some h,Html.LIBERAL,None -> "l" ^ md5  ^ h
+      | Some h,Html.CONSERVATIVE,Some m -> "C" ^ md5  ^ h ^ "\000" ^ m
+      | Some h,Html.MODERATE,Some m -> "M" ^ md5  ^ h ^ "\000" ^ m
+      | Some h,Html.LIBERAL,Some m -> "L" ^ md5 ^ h ^ "\000" ^ m
+      | None,_,Some m -> "X" ^ md5   ^ m
+    );
+    Render.render tmppath finalpath outtex md5 backcolor resolution_in_dpi
     end
 
 (* Arguments:
@@ -43,10 +43,11 @@ let render tmppath finalpath tree backcolor =
  * *)
 let _ =
     Texutil.set_encoding (try Sys.argv.(4) with _ -> "UTF-8");
-    try render Sys.argv.(1) Sys.argv.(2) (
-        Parser.tex_expr lexer_token_safe (
-            Lexing.from_string Sys.argv.(3))
-        ) (try Sys.argv.(5) with _ -> "rgb 1.0 1.0 1.0")
+    let resolution_in_dpi = (try int_of_string Sys.argv.(6) with _ -> 120) in
+        try render Sys.argv.(1) Sys.argv.(2) (
+            Parser.tex_expr lexer_token_safe (
+                Lexing.from_string Sys.argv.(3))
+             ) (try Sys.argv.(5) with _ -> "rgb 1.0 1.0 1.0") resolution_in_dpi
     with Parsing.Parse_error -> print_string "S"
        | LexerException _ -> print_string "E"
        | Texutil.Illegal_tex_function s -> print_string ("F" ^ s)
